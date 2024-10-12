c75f1e6753504cb8b56090067870501ad62437ed
Joaquin Arredondo <116196373+JoaquinArredondo@users.noreply.github.com>
2024-10-12 05:09:36 +0000

    added details for requirements

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -10,9 +10,11 @@ export default async function Home() {
 
   return (
     <div>
-      <h1>Hi</h1>
+      <h1>Hello Welcome! My Name is Joaquin Arredondo and this is some more about me! </h1>
+      <h1> PHONE: (510)-857-7068 | EMAIL: JoaquinAnthonyArredondo@gmail.com</h1>
       <div>This is my resume</div>
       <h2>Skills</h2>
+      <h1>Digitial Art, C, Software development, Video Game Development</h1>
       <h2>Projects</h2>
       <h2>Jobs</h2>
       <ol>
